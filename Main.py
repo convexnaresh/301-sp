ae892ed3e86be04095226998c1c53b1df7c2b760
Naresh Adhikari <convex.naresh@gmail.com>
2021-02-15 19:10:24 -0500

    added Main file.

---
+++ Main.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+print("This is a shared project that helps to learn differnt aspects of Secure Srogramming:\n")
+print("i) Python Programming\n"
+      "ii) Team Project Development using Git \n"
+      "iii) Secure Programming Practices \n")
+#Clone this project in your system using pycharm-charm project
+#Learn python basics by executing differnt files on your own.
+#Learn different secure programming skills, and use the skills on your group project.
