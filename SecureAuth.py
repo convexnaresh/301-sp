a1e260ee19da8c3ba713e6465b81b783d7100e77
Naresh Adhikari <convex.naresh@gmail.com>
2021-02-27 23:01:00 -0500

    added skeletal program for assignment-3.

--- SecureAuth.py
+++ SecureAuth.py
@@ -1,64 +1,50 @@
 '''
 Author: Naresh Adhikari, Sru
-This is skeletal program that students need to implement the declared
-and defined functions according to the logic/functional requirements stated in assigment-2.pdf.
+This is a skeletal program that students need to implement the declared
+and defined functions under @TODO annotation, according to the logic/functional requirements stated in assigment-2.pdf.
 
 Students are not expected to midify main() function.
 '''
 import hashlib
-
 def secure_hashed_passwd(username, hpasswd):
+    import hashlib, uuid
+    import os
+
     '''
     @TODO: Students are required to implement this function.
-
-    Stores given username and hpasswd in outfile after computing hash value of hpasswd input
     using salt+paper+sha3-512 algorithm
     :param username: string repr of username
     :param hpasswd: sha-3-152 string hash value
     :return: True if given values are stored successfully in outfile var; else returns False
     '''
-    outfile="hcredential.dat"
+
 
     #use salt and pepper to hash 'hpasswd' using sha-3-512 algorithm
+    # Add salt
+
+    # add pepper
+
+    #return salt,pepper,saltpepperdigest
 
 
-    #open file outfile in write mode.
-    #Write data in the file
-
-    #display a message "Credentials stored successfully"
-
-    #close the file
-
-    return True
-
-
-def verify_hashed_passwd(username, hpasswd):
+def verify_hashed_passwd(username, passwd):
     '''
     @TODO: Students are required to implement this function.
 
+    Server side verifies login credentials username and password
     :param username:
     :param hpasswd:
     :return:
     '''
-
-    infile="hcredential.dat"
-
+    #databse file with username and hashed-password.
+    infile="hlogins.dat"
     #open the file to read
-    #read the infile line by line to retrive a matching row with first
-    # field value equal to username supplied in this funciton.
-    # close the file
+    fd=open(infile,"r")
+    #read the infile line by line to retrive a matching row with first field value of username
 
-    #compute tempo_hash using salt,pepper (retrived above) and sha-3-512 algorithm
-
-
-    #check if tempo_hash and hpasswd matches.
-        #Display message if matches
-        #return True
-
-        #Display Message if NOT-matches
-        #return False
-
-    return True
+    #To read the file line by line, use a for loop.
+    #Hint: split each line by a comma "," to get list of username, salt, pepper, and stored_hashpassword values.
+    #implement other logics inside loop.
 
 def main():
     '''Do not modify this function.'''
@@ -66,33 +52,43 @@ def main():
     import hashlib, uuid
     import os
 
-    lusername=["shyamal@gmail.com","brutforce@yahoo.com","rainbow@sru.edu"]
-    lpasswd=["pass$1290Red","fail$567Blue","rainB0w159$"]
-    lhpasswd = []
+    lusername=["shyamal@gmail.com",
+                "brutforce@yahoo.com",
+                "lifegivesalot@protonmail.com",
+                "rainbow@sru.edu",
+                "ghana@makai.com",
+                "david@inst.edu",
+                "buttlerbusiness@sru.edu",
+                "myChurch45@state.edu"]
+    lpasswd=["pass$1290Red",
+            "fail$567Blue",
+            "rainB0w159$",
+            "lglot$$$Tatoo",
+            "ghana456$$909",
+            "DavI0234$09",
+            "IsBulltop345",
+            "xCrosTop24"]
 
-    print("Create account in servers for the following username and hash of user's password.")
-    #ClientSide:hash using salt+pepper+sha-3-512
-    for idx in range(0, len(lusername)):
-        #Add salt
-        salt = uuid.uuid4().bytes  # or .hex
-        HF = hashlib.sha224()
-        HF.update(str.encode(lpasswd[idx]))
-        HF.update(salt)  # or HF.update(bytes(salt,'utf-8'))
-
-        #add pepper
-        pepper = os.urandom(16)  # generate 3 bytes of random number.
-        HF.update(pepper)
-        saltpepperdigest = HF.hexdigest()
-        lhpasswd+=[saltpepperdigest]
-        #print("client-",idx,lusername[idx],salt,pepper,saltpepperdigest)
-        print(idx,",",lusername[idx],",",saltpepperdigest)
-
-    #call methods to store all of the values.
-    for i in range(0,len(lhpasswd)):
+    # open file outfile in write mode.
+    outfile="hlogins.dat"
+    fd = open(outfile, "w+")
+    #@server: call method for each usernames, passwords.
+    for i in range(0,len(lusername)):
         username=lusername[i]
-        hpasswd=lhpasswd[i]
-        secure_hashed_passwd(username,hpasswd)
-        verify_hashed_passwd(username,hpasswd)
+        passwd=lpasswd[i]
+        salt,pepper,saltpepperdigest=secure_hashed_passwd(username,passwd)
+        if i in [3,7,1]:continue
+        fd.write(username + "," + str(salt) + "," + str(pepper) + "," + saltpepperdigest+","+"$\n")
+    fd.close()
+
+    for j in range(0,len(lusername)):
+        uname=lusername[j]
+        passwd=lpasswd[j]
+        result=verify_hashed_passwd(uname,passwd)
+        if not result:
+            print("<!> Login failed for user ",uname)
+        else:
+            print("Login succesful for user ",uname)
 
 if __name__ == "__main__":
     main()
