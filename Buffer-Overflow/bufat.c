a4f19b06bf0afbda0371b609909aeb1195472831
git-naresh <naresh.adhikari@sru.edu>
2021-03-30 18:13:31 -0400

    sof files

---
+++ Buffer-Overflow/bufat.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+
+main(){
+       	char name[40];
+        printf("What is your name?\n");
+        gets(name);//scanf("%s", name);
+	
+        bo(name, "uname -a");
+}
+
+int bo(char *name, char *cmd){
+        char c[40];
+        char buffer[40];
+        
+	printf("Name buffer address:    %x\n", buffer);
+        printf("Command buffer address: %x\n", c);
+        
+	strcpy(c, cmd);
+        strcpy(buffer, name);
+        
+	printf("Goodbye, %s,%d\n", buffer,sizeof(buffer));
+
+        printf("Executing command:%s\n", c);
+        fflush(stdout);
+        system(c);
+}
